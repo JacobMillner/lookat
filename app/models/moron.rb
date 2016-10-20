39b05779372b0277173a935ab0018b1063ddb190
Jacob <jacob.millner@outlook.com>
2016-10-20 05:42:40 -0400

    Added validation for morons and removed img hover effects.

--- app/models/moron.rb
+++ app/models/moron.rb
@@ -1,2 +1,8 @@
 class Moron < ActiveRecord::Base
+  validates :imgur, :description, presence: true
+  validates :imgur, :description, uniqueness: true
+  validates :imgur, allow_blank: true, format: {
+    with: %r{\.(jpg|png)\Z}i,
+    message: 'Must be a URL for a JPG or PNG.'
+  }
 end
