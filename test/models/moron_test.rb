8bffb53f6ec3d827e21badfadab01dc60a5678fd
Jacob <jacob.millner@outlook.com>
2016-10-25 06:10:54 -0400

    added some more testing

--- test/models/moron_test.rb
+++ test/models/moron_test.rb
@@ -7,4 +7,23 @@ class MoronTest < ActiveSupport::TestCase
     assert moron.errors[:imgur].any?
     assert moron.errors[:description].any?
   end
+
+  test "URL must be a link to a PNG or JPG" do
+    moron = Moron.new(imgur:  "www.google.com",
+                      description: "My cool link to the googs")
+
+    assert moron.invalid?
+    assert_equal ["The URL must end in .jpg or .png"],
+      moron.errors[:imgur]
+
+    moron.imgur = "www.google.com/pics/logo.jpg"
+    assert moron.valid?
+  end
+
+  test "Description must be unique" do
+    moron = Moron.new(imgur: "test.jpg",
+                      description: morons(:goog).description)
+    assert moron.invalid?
+    assert_equal ["has already been taken"], moron.errors[:title]
+  end
 end
